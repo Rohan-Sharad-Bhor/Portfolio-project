f4d391a6394c39ebb75268d93edbcfdeaafecf59
Rohan Sharad Bhor <rsbhor2567@gmail.com>
2025-11-12 16:18:34 +0530

    Add files via upload

---
+++ portfolio.js
@@ -0,0 +1,10 @@
+
+
+
+ const typed = new Typed('.multiple-text', {
+      strings: ['Java Devloper', 'Frontend Devloper','Backend Devloper','SQL Devloper'],
+      typeSpeed: 80,
+      backSpeed:80,
+      backDelay:1200,
+      loop:true,
+    });
